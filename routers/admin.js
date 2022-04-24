d3a6a636541618fb8d29e5171bf80f156018c679
Khai <khaitqgch18519@fpt.edu.vn>
2022-04-24 23:57:59 +0700

    check role admin

--- routers/admin.js
+++ routers/admin.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 var controller = require('../controllers/admin');
-const upload = require("../utils/multer")
+const upload = require("../utils/multer");
+const {checkRoleAdmin} = require('../controllers');
 
-router.get("/", controller.getAdminPage);
+router.get("/", checkRoleAdmin,controller.getAdminPage);
 
-router.get("/mananger-book", controller.getManageBook);
+router.get("/mananger-book", checkRoleAdmin,controller.getManageBook);
 
 router.post("/store", upload.single('image'), controller.storeBook);
 
@@ -13,13 +14,13 @@ router.post("/delete-book", controller.deleteBook);
 
 router.post("/update-book",  controller.updateBook);
 
-router.get("/mananger-customer-order", controller.getManageOrder);
+router.get("/mananger-customer-order",checkRoleAdmin, controller.getManageOrder);
 
 router.post("/process-order", controller.processOrder);
 
 router.post("/delete-order", controller.deleteOrder);
 
-router.get("/feedback", controller.getFeedback);
+router.get("/feedback",checkRoleAdmin, controller.getFeedback);
 
 router.post("/rep-feedback", controller.storeFeedback);
 
